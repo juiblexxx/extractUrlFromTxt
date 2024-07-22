73c6d4a27d3335a36a56f67134e3be3a1aeec17b
yu <xxxyuxxx@gmail.com>
2024-07-22 16:50:03 +0900

    config.ini対応 GUI機能を統合

--- extractUrlFromTxt.py
+++ extractUrlFromTxt.py
@@ -4,30 +4,20 @@
 # ウィンドウが開いたらbrowseからテキストファイルを選択し
 # 実行ボタンを押すと、同じパスに_url.txtファイルが作成されます
 
-# モジュールをインポート
 import re
 import os
 import filecmp
 import configparser
 import requests
-
 import PySimpleGUI as sg
-
-# ライブラリをインポート
 from tkinter import messagebox
 
-config_path = "C:/Data/DL2/Python/extractUrlFromTxt"
-config_name = "config.ini"
+# 規定値の定義
+config_path = os.path.join(os.path.dirname(__file__), 'config.ini')
 
-line_url = ""
-line_token = ""
-
-output_file = "urls.txt"
-output_old_file = "urls_old.txt"
-
-# 指定したメッセージと画像を貼ってくれるLine通知用の関数
 def send_to_line_notify(url:str, token: str, notify_message: str):
     # def send_image_to_line_notify(url: str, token: str, notify_message: str, image_path: str):
+    # 指定したメッセージと画像を貼ってくれるLine通知用の関数
     headers = {"Authorization" : "Bearer "+ token}
     # with open(image_path, 'rb') as file:
     #     image = file.read()
@@ -37,109 +27,112 @@ def send_to_line_notify(url:str, token: str, notify_message: str):
     response = requests.post(url, headers=headers, params=payload)
     return response.status_code
 
-def extractUrl(inputpath, reference_text, charset):
-    # テキストファイルの存在チェック
-    if os.path.isfile(inputpath) :
-        pass
-    else:
-        messagebox.showinfo("ファイルがありません", inputpath)
-        return False
-
-    # テキストを開く
-    with open(inputpath, "r", encoding=charset) as f:
-        # with open(inputpath, "r") as f:
+def extract_url(input_path, charset) -> str:
+    # input_pathからURL文字列のみを返す
+    # ファイルを開く
+    with open(input_path, "r", encoding = charset) as f:
         text = f.read()
 
-    regex = r"https?://[\w/:%#\$&\?\(\)~\.=\+-]+"
-    urls = re.findall(regex, text)
+    regex = r"https?://[\w/:%#\$&\?\(\)~\.=\+-]+"   # URL用の正規表現
+    return re.findall(regex, text)
 
-    # with open(inputpath + "-url.txt", "w") as w:
-    with open(output_file, "a", encoding=charset) as w:
-        for url in urls:
-            if reference_text != "":
-                # 対象文字列指定があった場合は、存在した文字列のみ出力する
-                regex = reference_text
-                referenceUrl = re.findall(regex, url)
-                for rUrl in referenceUrl:
-                    w.write(f"{url}\n")
-            else:
-                w.write(f"{url}\n")
+def main_gui(target_path, target_string):
+    # チェックボックスグループの定義
+    CHECKBOX_UTF8 = "-CHECKBOX_UTF8-"
+    CHECKBOX_CP932 = "-CHECKBOX_CP932-"
+    CHECKBOX_GID01 = "-CHECKBOX_GID01-"
 
-# チェックボックスグループ
-CHECKBOX_UTF8 = "-CHECKBOX_UTF8-"
-CHECKBOX_CP932 = "-CHECKBOX_CP932-"
-CHECKBOX_GID01 = "-CHECKBOX_GID01-"
+    # PySimpleGUIウィンドウに配置する要素を定義
+    layout = [
+        [sg.Text("チェックしたいファイルを指定してください。\n同じフォルダで同じ拡張子をもつファイルをチェック対象とします。\nカンマ区切りで複数指定可")],
+        [sg.InputText(key="paths",default_text=target_path), sg.FileBrowse()],
+        [sg.Radio("UTF8", key=CHECKBOX_UTF8, group_id=CHECKBOX_GID01, default=True), sg.Radio("CP932", key=CHECKBOX_CP932, group_id=CHECKBOX_GID01)],
+        [sg.Text("探したい文字列（URLの一部）があれば指定してください\nカンマ区切りで複数指定可")],
+        [sg.InputText(key="references", default_text=target_string)],
+        [sg.Button("実行")]
+    ]
 
-# ウィンドウに配置する要素を定義
-layout = [
-    [sg.Text("URLを含んだテキストファイルを指定してください")],
-    [sg.InputText(key="url",default_text="C:/Data/DL2/[DL2-irvine]/Siki-win32-x64/data/profile/log/5ch.net/liveuranus/subject.json"), sg.FileBrowse()],
-    [sg.Radio("UTF8", key=CHECKBOX_UTF8, group_id=CHECKBOX_GID01, default=True), sg.Radio("CP932", key=CHECKBOX_CP932, group_id=CHECKBOX_GID01)],
-    [sg.Text("探したい文字列（URLの一部）があれば指定してください")],
-    [sg.InputText(key="reference", default_text="huggin")],
-    [sg.Button("実行")]
-]
+    # PySimpleGUIウィンドウを生成
+    window = sg.Window("extractUrlFromTxt", layout)
 
-# ウィンドウを作成
-window = sg.Window("extractUrlFromTxt", layout)
+    # UIループ
+    while True:
+        event, values = window.read()    # イベントと入力値を取得
+        if event == sg.WIN_CLOSED:  # ウィンドウが閉じられたら
+            break   # UIループ終了
 
-# イベントループ
-while True:
-    event, values = window.read()    # イベントと入力値を取得
-    if event == sg.WIN_CLOSED:  # ウィンドウが閉じられたら
-        break   # ループ終了
+        elif event == "実行":   # 実行ボタンが押されたら
+            main(values["paths"], values["references"])
+            messagebox.showinfo("完了", "処理完了しました。")
+            break   # UIループ終了
 
-    elif event == "実行":   # 実行ボタンが押されたら
-        # 比較対象ファイルの削除
-        # configparserの宣言とiniファイルの読み込み
-        config_ini = configparser.ConfigParser()
-        config_ini.read(os.path.join(config_path, config_name), encoding='utf-8')
+def main(target_path, target_string):
+    # メイン処理
+    # 古い前回結果ファイルを削除
+    if os.path.exists(output_old_file):
+        os.remove(output_old_file)
 
-        # LINE関係の情報をiniから取得
-        line_url = config_ini['LINE']['url']
-        line_token = config_ini['LINE']['token']
+    # 前回結果ファイルをリネーム
+    if os.path.exists(output_file) :
+        os.rename(output_file, output_old_file)
 
-        if os.path.exists(output_old_file) :
-            os.remove(output_old_file)
+    # チェック対象ファイルの文字コードセット
+    charset = "utf-8"
 
-        # 前回結果ファイルをリネーム
-        if os.path.exists(output_file) :
-            os.rename(output_file, output_old_file)
+    # チェック処理開始
+    for file_path in target_path.split(","):
+        if os.path.isfile(file_path):
+            # 基準拡張子とカレントパスを取得
+            base_extention = os.path.splitext(file_path)[1]
+            parent_path = os.path.dirname(file_path)
 
-        filepath = values["url"]    # テキスト入力値を取得
-        reference_text = values["reference"] # 探したい文字列を取得
+            # ディレクトリを取得してしまわないようisfileでファイルのみ取得
+            # かつ、base_extentionと同一拡張子のみ
+            # endswithでチェックするので大文字・小文字の区別あり
+            files = [
+                os.path.join(parent_path, file_name)
+                for file_name in os.listdir(parent_path)
+                if os.path.isfile(os.path.join(parent_path, file_name)) and file_name.endswith(base_extention)
+                ]
 
-        # 文字コード判定
-        charset = "utf-8"
-        if values[CHECKBOX_UTF8]:
-            charset = "utf-8"
-        elif values[CHECKBOX_CP932]:
-            charset = "cp932"
+            with open(output_file, "a", encoding = charset) as w:
+                # 見つかったファイルからURL文字列を取得する
+                # URLの絞り込み指定があったなら、絞り込んだURLのみファイルに出力する
+                for file in files:
+                    urls = extract_url(file, charset)
+                    for url in urls:
+                        if target_string != "":
+                            # 対象文字列指定があった場合は、存在した文字列のみ出力する
+                            for regex in target_string.split(","):
+                                reference_url = re.findall(regex, url)
+                                for ret in reference_url:   # ここでマッチした箇所は無視、元URLを出力したい
+                                    w.write(f"{url}\n")
+                        else:
+                            w.write(f"{url}\n")
 
-        if os.path.isfile(filepath):
-            # 振分先ディレクトリパスを作成する為の親パスとして取得しておく
-            parent_path = os.path.dirname(filepath)
-            # messagebox.showinfo("", parent_path)
-            # for file_name in os.listdir(parent_path):
-            #     if os.path.isfile(os.path.join(parent_path, file_name)):
-            #         print(f"{parent_path}/{file_name}")
+    # 出力ファイルのdiffとる
+    if filecmp.cmp(output_file, output_old_file, shallow = False) == False and line_enable == "1":
+        send_to_line_notify(line_url, line_token, "差分あり")
 
-            # 同一ディレクトリ内にあるファイルを取得
-            files = [os.path.join(parent_path, file_name) for file_name in os.listdir(parent_path) if os.path.isfile(os.path.join(parent_path, file_name))]  # ディレクトリを取得してしまわないようisfileでファイルリストを取取得
-            # print(f"{files}")
-            # 同一拡張子のファイルのみ検出したいので代表ファイルの拡張子を取得
-            base_extention = os.path.splitext(filepath)[1]
-            for file in files:
-                # 拡張子チェック
-                file_extention = os.path.splitext(file)[1]
-                if file_extention == base_extention:
-                    # messagebox.showinfo("debug", file)
-                    extractUrl(file, reference_text, charset)
-                    # break   # ループ終了
 
-            # 出力ファイルのdiffとる
-            if filecmp.cmp(output_file, output_old_file, shallow=False) == False:
-                send_to_line_notify(line_url, line_token, "差分あり")
 
-        # messagebox.showinfo("完了", "出力しました。")
-        break
+# コンフィグの取得
+config_ini = configparser.ConfigParser()
+config_ini.read(config_path, encoding='utf-8')
+
+# コンフィグ値を変数に代入しておく
+gui = config_ini["SETTINGS"]["gui"]
+output_file = config_ini["SETTINGS"]["output_new"]
+output_old_file = config_ini["SETTINGS"]["output_old"]
+target_path = config_ini["SETTINGS"]["target_path"]
+target_string = config_ini["SETTINGS"]["target_string"]
+line_enable = config_ini["LINE"]["enable"]
+line_url = config_ini["LINE"]["url"]
+line_token = config_ini["LINE"]["token"]
+
+if __name__ == "__main__":
+    if gui == "1":
+        main_gui(target_path, target_string)
+    else:
+        main(target_path, target_string)
+
